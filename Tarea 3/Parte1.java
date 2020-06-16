9a0d739ce97f23e285b46fdbbbf5c4edf239a191
luciofondon98 <luciofondon98@gmail.com>
2020-06-16 14:59:22 -0400

    parte 1 tarea3 lista

---
+++ Tarea 3/Parte1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class Parte1{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int mayor_divisor = 1, mayor_cant_divisores = 1;
+		scan.close();
+		final long startTime = System.nanoTime();
+		for (int i = 2; i <= N; i++){
+			int divisores_i = 0;
+			for (int j = 2; j < i; j++){
+				if (i % j == 0){
+					divisores_i = divisores_i + 1;
+				}
+			}
+			if (divisores_i > mayor_cant_divisores){
+				mayor_cant_divisores = divisores_i;
+				mayor_divisor = i;
+			}
+		}
+		System.out.println("El número con la mayor cantidad de divisores entre 1 y " + N + " es: " + mayor_divisor);
+		final long endTime = System.nanoTime();
+		long totalTime = endTime - startTime;
+		System.out.println("El tiempo que toma en ejecutarse el programa en segundos es de: " + totalTime/1000000000f + " segundos.");
+	}
+}
+
+  
